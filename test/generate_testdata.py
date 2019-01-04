3ce64fa57292b4dd3ea84e249ce0e7f04bbdff2c
Simeon Doetsch <science@simske.com>
2019-01-04 14:38:09 +0100

    Added IO test

---
+++ test/generate_testdata.py
@@ -0,0 +1,14 @@
+
+
+def generate_gridfile(filename='grid.out', dims=(10,9,8)):
+    with open(filename, 'w') as f:
+        f.write('# FILE HEADER\n')
+
+        for res in dims:
+            f.write(f'{res}\n')
+            for i in range(res):
+                f.write(f' {i}   {i:.12e}    {i+1:.12e}\n')
+
+
+if __name__ == '__main__':
+    generate_gridfile()
---
+++ test/test_io.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+import numpy as np
+from plutoplot.io import Grid
+from generate_testdata import generate_gridfile
+
+class TestGrid(unittest.TestCase):
+
+    def setUp(self):
+        self.file = 'gridtest.out'
+        self.dims = (10, 11, 12)
+        generate_gridfile(self.file, self.dims)
+        self.grid = Grid(self.file)
+
+    def tearDown(self):
+        os.remove(self.file)
+    
+    def testDimensions(self):
+        self.assertEqual(self.dims, self.grid.dims)
+        self.assertEqual(self.dims, self.grid.data_shape)
+    
+    def testData(self):
+        for i, res in enumerate(self.dims, start=1):
+            x = getattr(self.grid, f'x{i}')
+            dx = getattr(self.grid, f'dx{i}')
+            np.testing.assert_array_almost_equal_nulp(x, np.arange(0.5, res, 1))
+            np.testing.assert_array_almost_equal_nulp(dx, np.ones(res))
+
+
+if __name__ == '__main__':
+    unittest.main()
