717206ba12ef9f36f64f6806442a4310eee2bd8b
Simeon Doetsch <science@simske.com>
2020-11-03 16:42:43 +0100

    Remove double import of version in __init__.py

--- plutoplot/__init__.py
+++ plutoplot/__init__.py
@@ -7,7 +7,3 @@ from .simulation import Simulation
 from . import misc
 
 from matplotlib.pyplot import show
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
