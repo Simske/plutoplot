7eef1feaf138c15542a39296829e138402db07a2
Simske <mail@simske.com>
2022-04-01 21:48:08 +0200

    Fix __version__ inference for Python <3.8

--- plutoplot/__init__.py
+++ plutoplot/__init__.py
@@ -1,7 +1,14 @@
-import importlib.metadata
+try:
+    import importlib.metadata
 
-__version__ = importlib.metadata.version("plutoplot")
-del importlib
+    __version__ = importlib.metadata.version("plutoplot")
+    del importlib
+except ImportError:  # Python <3.8
+    import pkg_resources
+
+    __version__ = pkg_resources.get_distribution("plutoplot").version
+    del pkg_resources
+
 
 from . import misc
 from .grid import Grid
